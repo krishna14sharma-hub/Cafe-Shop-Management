92e6f1b877592e6e003d0babb21975eaf9117863
Krishna Sharma <ks1166180@gmail.com>
2026-03-22 20:16:22 +0530

    push the code for get and post with the help of Swagger

---
+++ Controllers/OrderCOntroller.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Cafe_management_1.Models;
+
+namespace Cafe_management_1.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OrderCOntroller : ControllerBase
+    {
+        private static List<Order> Orders = new List<Order>();
+
+        [HttpPost]
+        public IActionResult PlaceOrder(Order order)
+        {
+            Orders.Add(order);
+            return Ok("Order Placed Successfully");
+        }
+
+        [HttpGet]
+        public IActionResult GetOrders()
+        {
+            return Ok(Orders);
+        }
+    }
+}
---
+++ Models/Order.cs
@@ -0,0 +1,10 @@
+﻿namespace Cafe_management_1.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string ItemName { get; set; }
+        public int Quantity { get; set; }
+        
+    }
+}
