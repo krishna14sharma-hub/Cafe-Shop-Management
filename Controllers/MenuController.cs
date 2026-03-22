f544f2bd52838ebb97422e1dc95bddaf4eda34da
Krishna Sharma <ks1166180@gmail.com>
2026-03-22 19:05:53 +0530

    initial commit

---
+++ Controllers/MenuController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cafe_management_1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MenuController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetMenu()
+        {
+            var menu = new List<object>
+            {
+                new {Id = 1 , Name = "Coppuccino" , Price = 120 },
+                new { Id = 2 , Name = "Latte " , Price = 150} ,
+                new {Id = 3 , Name = "Espresso " , Price = 100}
+            };
+            return Ok(menu);
+        }
+    }
+}
